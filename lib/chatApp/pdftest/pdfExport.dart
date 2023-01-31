8d7f4dcec7c098d8728e3780994eecc3f8f7a6bf
vegaKannur12 <100991705+vegaKannur12@users.noreply.github.com>
2023-01-31 16:57:44 +0530

    pdf

--- lib/chatApp/pdftest/pdfExport.dart
+++ lib/chatApp/pdftest/pdfExport.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 
 import 'package:bestengineer/chatApp/pdftest/pdfmodel.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 import 'package:share_plus/share_plus.dart';
 
 class ExportPdf {
@@ -13,10 +16,33 @@ class ExportPdf {
 
   Future<Uint8List> makePdf(Invoice invoice) async {
     final pdf = pw.Document();
+    final image = await imageFromAssetBundle('assets/noImg.png');
 
     pdf.addPage(
       pw.Page(
           build: (context) => pw.Column(children: [
+                pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                  children: [
+                    pw.Column(
+                      children: [
+                        pw.Text("Attention to: ${invoice.customer}"),
+                        pw.Text(invoice.address),
+                      ],
+                      crossAxisAlignment: pw.CrossAxisAlignment.start,
+                    ),
+                    pw.SizedBox(
+                      height: 150,
+                      width: 150,
+                      child: pw.Image(
+                        image,
+                      ),
+                    )
+                  ],
+                ),
+                pw.SizedBox(
+                  height: 50,
+                ),
                 pw.Table(
                   border: pw.TableBorder.all(color: PdfColors.black),
                   children: [
@@ -32,21 +58,14 @@ class ExportPdf {
                         ),
                       ],
                     ),
-                    // The remaining rows contain each item from the invoice, and uses the
-                    // map operator (the ...) to include these items in the list
+
                     ...invoice.items.map(
-                      // Each new line item for the invoice should be rendered on a new TableRow
                       (e) => pw.TableRow(
                         children: [
-                          // We can use an Expanded widget, and use the flex parameter to specify
-                          // how wide this particular widget should be. With a flex parameter of
-                          // 2, the description widget will be 66% of the available width.
                           pw.Expanded(
                             child: pw.Text(e.description),
                             flex: 2,
                           ),
-                          // Again, with a flex parameter of 1, the cost widget will be 33% of the
-                          // available width.
                           pw.Expanded(
                             child: pw.Text("\$${e.cost}"),
                             flex: 1,
@@ -54,8 +73,7 @@ class ExportPdf {
                         ],
                       ),
                     ),
-                    // After the itemized breakdown of costs, show the tax amount for this invoice
-                    // In this case, it's just 10% of the invoice amount
+
                     pw.TableRow(
                       children: [
                         pw.Text('TAX', textAlign: pw.TextAlign.right),
@@ -80,18 +98,39 @@ class ExportPdf {
                 ),
               ])),
     );
-
-    // savedocument(name: "m.pdf", pdf: pdf);
     return pdf.save();
   }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   Future<File> savePdf(Invoice invoice) async {
     final pdf = pw.Document();
+    final image = await imageFromAssetBundle('assets/noImg.png');
 
     pdf.addPage(
       pw.Page(
           build: (context) => pw.Column(children: [
+                pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                  children: [
+                    pw.Column(
+                      children: [
+                        pw.Text("Attention to: ${invoice.customer}"),
+                        pw.Text(invoice.address),
+                      ],
+                      crossAxisAlignment: pw.CrossAxisAlignment.start,
+                    ),
+                    pw.SizedBox(
+                      height: 150,
+                      width: 150,
+                      child: pw.Image(
+                        image,
+                      ),
+                    )
+                  ],
+                ),
+                pw.SizedBox(
+                  height: 50,
+                ),
                 pw.Table(
                   border: pw.TableBorder.all(color: PdfColors.black),
                   children: [
